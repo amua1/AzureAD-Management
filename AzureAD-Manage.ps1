5f9de9ed60395cb08e39b8e6907758ade6365998
amua <68795999+amua1@users.noreply.github.com>
2021-01-24 12:03:42 +0100

    #2 Added user recover option
    #2 Added user recover option , changed the script to check whether required modules (AzureAD and MSOnline) are installed, added the block which checks whether the user is Authenticated to Azure AD or not

--- AzureAD-Manage.ps1
+++ AzureAD-Manage.ps1
@@ -4,17 +4,43 @@
 clear
 Write-Host "This script helps you do basic user and group management in Azure AD"
 
-# This line checks whether the user is connected to Azure or not
-$checkifuserisconnected = Read-Host "Have you connected recently to Azure AD ,
-1. YES
-2. NO
-"
+#b001 ---This block checks whether the required modules are installed---#
+Write-Host "Checking whether the required modules (AzureAD and MSOnline) are installed"
 
-If ($checkifuserisconnected -eq 2)
+If (-not(Get-InstalledModule -Name AzureAD -ErrorAction silentlycontinue)) {
+  Write-Host "Azure AD module is not installed - Installing ..."
+  Install-Module -Name AzureAD 
+  Import-Module -Name AzureAD
+  clear
+}
+Else {
+  Write-Host "Azure AD module is already installed"
+}
+
+If (-not(Get-InstalledModule -Name MSOnline -ErrorAction silentlycontinue)) {
+  Write-Host "MSOnline module is not installed - Installing ..."
+  Install-Module -Name MSOnline
+  Import-Module -Name MSOnline
+  clear
+}
+Else {
+  Write-Host "MSOnline module is already installed"
+}
+
+#end of b001
+
+#b002 --- This block checks whether the user is connected to Azure or not ---#
+
+try 
+{
+    Get-AzureADTenantDetail
+}
+catch  [Microsoft.Open.Azure.AD.CommonLibrary.AadNeedAuthenticationException]
 {
     Connect-AzureAD
 }
-
+clear
+#end of b002
 
 while ($True)
 {
@@ -31,7 +57,7 @@ clear
 if ($categorysel -eq 1)
 {
     Write-Host "You have choosen the USER MANAGEMENT section - Here you can Add , Delete , Modify and Recover deleted users"
-    Write-Host "Choose one of the options provided below"
+    Write-Host "Choose one of the options provided below:"
     $usermanagementsel = Read-Host "
         1. Add Users
         2. Invite Users
@@ -54,6 +80,10 @@ if ($categorysel -eq 1)
     elseif ($usermanagementsel -eq 4)
     {
         .\usermanagement\usermodify.ps1
+    }
+    elseif ($usermanagementsel -eq 5)
+    {
+        .\usermanagement\userrestore.ps1
     }
 }
 #other settings to be added later
---
+++ usermanagement/userrestore.ps1
@@ -0,0 +1,18 @@
+﻿##########################################################
+#  Single user recover script - Azure AD                                        #
+##########################################################
+
+clear
+Write-Host "Here is a list of deleted users that can be recovered"
+Get-MsolUser -ReturnDeletedUsers | Select-Object UserPrincipalName,DisplayName
+$username = Read-Host "Enter the username of the user that you need to recover"
+Try
+{
+    Restore-MsolUser -UserPrincipalName $username -AutoReconcileProxyConflicts
+    Write-Host "User was restored successfully"
+    clear
+}
+Catch
+{
+    Write-Host "Unexpected error"
+}
